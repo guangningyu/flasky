4a69a0c9710721edf8412abfb3f74daaeff2697b
Guangning Yu <hi@guangningyu.com>
2017-04-25 19:16:35 +0800

    test basic concepts

--- app.py
+++ app.py
@@ -2,6 +2,11 @@
 
 import os
 from flask import Flask
+# There are two contexts in Flask: the application context and the request context.
+from flask import request
+from flask import make_response
+from flask import redirect
+from flask import abort
 
 # Flask uses the __name__ argument to determine the root path of the application so that
 # it later can find resource files relative to the location of the application.
@@ -9,11 +14,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return '<h1>Hello World!</h1>'
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/browser')
+def get_browser():
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Your browser is %s</h1>' % user_agent
+
+@app.route('/set_cookie')
+def set_cookie():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+
+@app.route('/redirect')
+def test_redirect():
+    return redirect('http://www.bing.com')
+
+@app.route('/abort')
+def test_abort():
+    abort(404)
+    return '<h1>You should not see this.</h1>'
+
 
 if __name__ == '__main__':
     host = os.environ.get('HOST', '0.0.0.0')
